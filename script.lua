06756c71c88d21145b55d7328956c0d052584527
x2hryyx <130052782+RevizxDD@users.noreply.github.com>
2023-11-05 10:59:14 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,5 +1,5 @@
 repeat wait() until game:IsLoaded();
-if game.PlaceId == 13772394625 or game.PlaceId == 15185247558 or game.PlaceId == 14915220621 then
+if game.PlaceId == 13772394625 or game.PlaceId == 15185247558 or game.PlaceId == 14915220621 or game.PlaceId == 14732610803 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/RevizxDD/kak/main/BladeBallScript.lua"))()
 else
     game.Players.LocalPlayer:Kick("Map not found !")
@@ -7,7 +7,7 @@ else
 
 
 
-
+wait(2)
 
 
   
